66b43dbdf496839404e9f479ad4cff264e9548b3
shahbajkhan2023 <rk2403476@gmail.com>
2024-10-10 16:00:31 +0530

    auth token authentication in drf

---
+++ token_auth/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("api-token-auth/", views.HelloView.as_view()),
+    path("api/auth/register/", views.RegistrationView.as_view()),
+]
+
+
